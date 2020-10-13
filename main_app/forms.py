a9c0e79b1450b614c820d49620340478d161bfb0
cem aytac <cenko@MacBook-Air-3.local>
2020-10-12 21:49:14 -0400

    slight update to navbar

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,5 +1,7 @@
 from django.forms import ModelForm
 from .models import Stat
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class StatForm(ModelForm):
@@ -7,3 +9,10 @@ class StatForm(ModelForm):
         model = Stat
         fields = ['date', 'goals', 'assists',
                   'clean_sheets', 'shots']
+
+
+# class SignupForm(UserCreationForm):
+#     first_name = forms.CharField(max_length=30)
+#     last_name = forms.CharField(max_length=30)
+#     email = forms.EmailField(max_length=254)
+#     team =
