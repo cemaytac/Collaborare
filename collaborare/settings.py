ad056321a27db29a4a2c54f202454d80d3789e78
cem aytac <cenko@MacBook-Air-3.local>
2020-10-14 16:49:49 -0400

    config deployment

--- collaborare/settings.py
+++ collaborare/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_tables2',
 ]
 
 MIDDLEWARE = [
@@ -72,7 +71,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                "django.template.context_processors.request",
             ],
         },
     },
